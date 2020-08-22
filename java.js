c44c50deb79ec33b6f45a42b01f3ebd9bfb02516
Todd Murdoch <TheClash090@gmail.com>
2020-08-22 09:51:15 -0700

    removed pw.css and renamed to style.css, added html background, adjust font sizes and password gen box to fit screen.

--- java.js
+++ java.js
@@ -1,6 +1,10 @@
+/* Variable of characters upper/lower, numbers, and symbols */
+
 var char = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
 var num = '0123456789';
 var sym = '!@#$%^&*=-_';
+
+/* Elements corresponding with html IDs */
 
 var charNum = document.getElementById("charNum");
 var numBox = document.getElementById("num");
@@ -8,7 +12,7 @@ var symBox = document.getElementById("sym");
 var submit = document.getElementById("submit");
 var yourPw = document.getElementById("yourPw");
 
-
+/* Events when clicking number box, symbol box, and submit buttons with "on-click function e as declared but not read:"*/
 submit.addEventListener("click",function(e){
     var characters = char;
     (numBox.checked) ? characters += num : '';
